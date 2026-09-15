f1ebdb709a563210e3207cf7e93260a333c590e2
Lehlohonolo Sethibe <LehlohonoloSethibe@outlook.com>
2026-09-15 19:32:33 +0200

    Refreshed Manage Donations view with custom table styling, updated property bindings, added DAF logo to navigation bar, and integrated separate CSS stylesheet.

--- DisasterAlleviationApp/obj/Debug/net10.0/DisasterAlleviationApp.AssemblyInfo.cs
+++ DisasterAlleviationApp/obj/Debug/net10.0/DisasterAlleviationApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DisasterAlleviationApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a54dd49a114cc6eac3ac1905a3949b2f83e3cab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28e4c1fb525063c5f2b3df0847573daa2ef8fef0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DisasterAlleviationApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DisasterAlleviationApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
